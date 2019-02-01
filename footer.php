b472ebc5bbfe512b1fb516ac350bf3fef7e19464
Randy Ferrer <randfer@gmail.com>
2019-01-31 19:35:17 -0500

    class project

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Footer template partial
+ * 
+ * @package mytheme_scratch_theme
+ * 
+ */
+?>
+
+</div>
+<!-- /.row -->
+</div>
+<!-- /.container -->
+
+<!-- Footer -->
+<footer>
+  <div class="container">
+    <div class=row>
+      <div class="col-lg-8 col-md-10 mx-auto">
+      </div>
+    </div><!-- /.row -->
+  </div><!-- /.container -->
+</footer><!-- /.footer -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
